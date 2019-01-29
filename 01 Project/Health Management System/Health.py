5e4f6626f82fc5334b4e2b11529df2a31d3c617e
Rahul Shukla <ne.2rahulshukla@gmail.com>
2019-01-29 16:27:05 +0530

    Mini-Project

--- 01 Project/Health Management System/Health.py
+++ 01 Project/Health Management System/Health.py
@@ -1,5 +1,6 @@
 # Health Management System
-#this system lock your diet & exercise Routine
+#this system log your diet & exercise Routine
+
 
 #---------------------------------written time------------------------------------------#
 def getdate():
@@ -36,74 +37,119 @@ def hammad_WE(name,x):
         print("\n----> You have written successfully <------")
 
 #---------------------------------Read data------------------------------------------------#
+
 def rohan_RD(name,x):
-    with open("rohan_diet.txt") as f:
-        a=f.read()
-        print(" Your gathered information \n" + a)
+    try:
+        with open("rohan_diet.txt") as f:
+            a=f.read()
+            print(" Your gathered information \n" + a)
+    except Exception as e:
+        print(e)
+        print("Please create a data of user before retrieve")
 def rohan_RE(name,x):
-    with open("rohan_exercise.txt") as f:
-        a = f.read()
-        print(" Your gathered information \n" + a)
+    try:
+        with open("rohan_exercise.txt") as f:
+            a = f.read()
+            print(" Your gathered information \n" + a)
+    except Exception as e:
+        print(e)
+        print("Please create a data of user before retrieve")
 
 
 def harry_RD(name,x):
-    with open("harry_diet.txt") as f:
-        a = f.read()
-        print(" Your gathered information \n" + a)
+    try:
+        with open("harry_diet.txt") as f:
+            a = f.read()
+            print(" Your gathered information \n" + a)
+    except Exception as e:
+        print(e)
+        print("Please create a data of user before retrieve")
 def harry_RE(name,x):
-    with open("harry_exercise.txt") as f:
-        a = f.read()
-        print(" Your gathered information \n" + a)
+    try:
+        with open("harry_exercise.txt") as f:
+            a = f.read()
+            print(" Your gathered information \n" + a)
+    except Exception as e:
+        print(e)
+        print("Please create a data of user before retrieve")
 
 def hammad_RD(name,x):
-    with open("hammad_diet.txt") as f:
-        a = f.read()
-        print(" Your gathered information \n" + a)
+    try:
+        with open("hammad_diet.txt") as f:
+            a = f.read()
+            print(" Your gathered information \n" + a)
+    except Exception as e:
+        print(e)
+        print("Please create a data of user before retrieve")
 def hammad_RE(name,x):
-    with open("hammad_exercise.txt") as f:
-        a = f.read()
-        print(" Your gathered information \n" + a)
+    try:
+        with open("hammad_exercise.txt") as f:
+            a = f.read()
+            print(" Your gathered information \n" + a)
+    except Exception as e:
+        print(e)
+        print("Please create a data first before retrieve")
 
 #---------------------------------Take input------------------------------------------------#
+while True:
+    try:
+        decision = int(input("What you want to do-->\n1 for write, 2 for retrieve :\n "))
+        break
+    except Exception as e:
+        print(e)
+        print("Please enter valid input")
 
-decision = input("What you want to do-->\n1 for write, 2 for retrieve : ")
-if decision == "1":
-    name = input("Press --> 1 for Rohan,\n2 for Harry,\n3 for Hammad:\n" )
-    x = input("Press --> 1 for Diet, 2 for for Exercise: ")
-    if name == "1" and x == "1":
+if decision == 1:
+    while True:
+        try:
+            name = int(input("Press --> 1 for Rohan,\n2 for Harry,\n3 for Hammad:\n" ))
+            x    = int(input("Press --> 1 for Diet, 2 for for Exercise: "))
+            break
+        except Exception as e:
+            print(e)
+            print("Please enter valid input")
+
+    if name == 1 and x == 1:
         rohan_WD(name,x)
-    elif name == "1" and x == "2":
+    elif name == 1 and x == 2:
         rohan_WE(name,x)
 
 
-    if name == "2" and x == "1":
+    if name == 2 and x == 1:
         harry_WD(name,x)
-    elif name == "2" and x == "2":
+    elif name == 2 and x == 2:
         harry_WE(name,x)
 
 
-    if name == "3" and x == "1":
+    if name == 3 and x == 1:
         hammad_WD(name,x)
-    elif name == "3" and x == "2":
+    elif name == 3 and x == 2:
         hammad_WE(name,x)
 
 else:
-    name = input("Press --> 1 for Rohan,\n2 for Harry,\n3 for Hammad:\n")
-    x = input("Press --> 1 for Diet, 2 for for Exercise: ")
+    while True:
+        try:
+            name = int(input("Press --> 1 for Rohan,\n2 for Harry,\n3 for Hammad:\n"))
+            x    = int(input("Press --> 1 for Diet, 2 for for Exercise: "))
+            break
+        except Exception as e:
+            print(e)
+            print("Please enter valid input")
 
-    if name == "1" and x == "1":
+
+    if name == 1 and x == 1:
         rohan_RD(name, x)
-    elif name == "1" and x == "2":
+    elif name == 1 and x == 2:
         rohan_RE(name, x)
 
-    if name == "2" and x == "1":
+    if name == 2 and x == 1:
         harry_RD(name, x)
-    elif name == "2" and x == "2":
+    elif name == 2 and x == 2:
         harry_RE(name, x)
 
-    if name == "3" and x == "1":
+    if name == 3 and x == 1:
         hammad_RD(name, x)
-    elif name == "3" and x == "2":
+    elif name == 3 and x == 2:
         hammad_RE(name, x)
 
 
